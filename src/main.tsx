fd97ae4e6ea7abca5f6495f4ff9efd78385828d0
SergeyV1S <v1s.coop@yandex.ru>
2025-06-13 13:44:57 +0500

    initialization

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+
+import "./assets/index.css";
+
+const App = () => <div className=''>f</div>;
+
+createRoot(document.getElementById("root")!).render(<App />);
